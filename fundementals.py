f1bd056045edbc7fb39db84585477bacd16b1629
bymoen <alexb@innotechcollege.com>
2023-05-25 19:50:37 -0600

    Added simple functions

--- fundementals.py
+++ fundementals.py
@@ -19,3 +19,24 @@ elif(my_num > 100 or my_bool == True):
     print('Large or True')
 else:
     print("I don't know")
+
+my_strings = ['This is the first string','This is the second string','This is the third string']
+my_nums = [1,2,3,4,5,6]
+
+for string in my_strings:
+    print(string)
+
+for num in my_nums:
+    print(f'Look at this number: {num}')
+
+def static_greeting():
+    print('Hello Alex')
+
+static_greeting()
+
+def dynamic_greeting(to_greet):
+    print('Hello', to_greet)
+
+dynamic_greeting('Bob')
+dynamic_greeting('Alice')
+dynamic_greeting('Rob')
